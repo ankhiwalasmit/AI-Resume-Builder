c50e750a99ed2c4df0822030d87bc52579d46506
ankhiwalasmit <ankhiwalasmit@gmail.com>
2024-09-27 19:53:43 +0530

    final commit

--- src/dashboard/components/ResumeCardItem.jsx
+++ src/dashboard/components/ResumeCardItem.jsx
@@ -65,7 +65,7 @@ function ResumeCardItem({resume,refreshData}) {
               </div>
         </div>
         </Link>
-        <div className='border p-3 flex justify-between  text-black rounded-b-lg shadow-lg'
+        <div className='border p-3 flex justify-between  text-white rounded-b-lg shadow-lg'
          style={{
           background:resume?.themeColor
         }}>
