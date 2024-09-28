18d7d2ac46120d01a257fc64777ede69177e989b
ankhiwalasmit <ankhiwalasmit@gmail.com>
2024-09-28 18:22:18 +0530

    homepage updated

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -101,15 +101,6 @@ function Home() {
 
     
     </div>
-
-    <div className="mt-12 text-center">
-      <a
-        href="/sign-in"
-        className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
-      >
-        Get Started Today
-      </a>
-    </div>
     </section>
   </div>
  
