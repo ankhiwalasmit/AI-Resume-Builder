7c87c54ff0ff33179a3ba019ab2c3e261d9c4a11
ankhiwalasmit <ankhiwalasmit@gmail.com>
2024-09-30 23:40:05 +0530

    improve ai prompt

--- src/dashboard/resume/components/RichTextEditor.jsx
+++ src/dashboard/resume/components/RichTextEditor.jsx
@@ -5,7 +5,7 @@ import React, { useContext, useState } from 'react'
 import { BtnBold, BtnBulletList, BtnClearFormatting, BtnItalic, BtnLink, BtnNumberedList, BtnStrikeThrough, BtnStyles, BtnUnderline, Editor, EditorProvider, HtmlButton, Separator, Toolbar } from 'react-simple-wysiwyg'
 import { AIChatSession } from './../../../../service/AIModal';
 import { toast } from 'sonner';
-const PROMPT='Provide a concise summary of my experience in {positionTitle}. Omit any mention of the role title or use of special characters. Ensure the output is in paragraph format and suitable for inclusion in a resume.'
+const PROMPT='position titile: {positionTitle} , Depends on position title give me 5-7 bullet points for my experience in resume (Please do not add experince level and No JSON array) , give me result in HTML tags'
 function RichTextEditor({onRichTextEditorChange,index,defaultValue}) {
     const [value,setValue]=useState(defaultValue);
     const {resumeInfo,setResumeInfo}=useContext(ResumeInfoContext)
