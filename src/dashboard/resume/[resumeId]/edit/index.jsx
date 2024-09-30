6991a6528d575a13122c5a26889552e906afa3e7
ankhiwalasmit <ankhiwalasmit@gmail.com>
2024-09-30 14:14:03 +0530

    global api

--- src/dashboard/resume/[resumeId]/edit/index.jsx
+++ src/dashboard/resume/[resumeId]/edit/index.jsx
@@ -4,7 +4,7 @@ import { ResumeInfoContext } from '@/context/ResumeInfoContext'
 import ResumePreview from '@/dashboard/resume/components/ResumePreview'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import GlobalApi from './../../../../service/GlobalApi'
+import GlobalApi from './../../../../../service/GlobalApi'
 
 function ViewResume() {
 
