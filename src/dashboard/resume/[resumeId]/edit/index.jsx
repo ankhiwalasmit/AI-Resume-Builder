7cc539486afe712395584251cbe173c3e9055fd4
ankhiwalasmit <ankhiwalasmit@gmail.com>
2024-09-30 14:11:47 +0530

    view resume

--- src/dashboard/resume/[resumeId]/edit/index.jsx
+++ src/dashboard/resume/[resumeId]/edit/index.jsx
@@ -1,37 +1,70 @@
+import Header from '@/components/custom/Header'
+import { Button } from '@/components/ui/button'
+import { ResumeInfoContext } from '@/context/ResumeInfoContext'
+import ResumePreview from '@/dashboard/resume/components/ResumePreview'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import FormSection from '../../components/FormSection';
-import ResumePreview from '../../components/ResumePreview';
-import { ResumeInfoContext } from '@/context/ResumeInfoContext';
-import dummy from '@/data/dummy';
-import GlobalApi from './../../../../../service/GlobalApi';
+import GlobalApi from './../../../../service/GlobalApi'
 
-function EditResume() {
-    const {resumeId}=useParams();
-    const [resumeInfo,setResumeInfo]=useState();
-    useEffect(()=>{
-       
+function ViewResume() {
+
+    const [resumeInfo, setResumeInfo] = useState();
+    const { resumeId } = useParams();
+
+    useEffect(() => {
         GetResumeInfo();
-    },[])
+    }, [])
 
-
-    const GetResumeInfo=()=>{
-        GlobalApi.GetResumeById(resumeId).then(resp=>{
-          console.log(resp.data.data);
-          setResumeInfo(resp.data.data);
+    const GetResumeInfo = () => {
+        GlobalApi.GetResumeById(resumeId).then(resp => {
+            console.log(resp.data.data);
+            setResumeInfo(resp.data.data);
         })
     }
 
-  return (
-    <ResumeInfoContext.Provider value={{resumeInfo,setResumeInfo}}>
-    <div className='grid grid-cols-1 md:grid-cols-2 p-10 gap-10'>
-        {/* Form Section  */}
-          <FormSection/>
-        {/* Preview Section  */}
-         <ResumePreview/>
-    </div>
-    </ResumeInfoContext.Provider>
-  )
+    const HandleDownload = () => {
+        window.print();
+    }
+
+    const HandleShare = () => {
+        if (navigator.share) {
+            navigator.share({
+                title: resumeInfo?.firstName + " " + resumeInfo?.lastName + " resume",
+                text: "Hello Everyone, This is my resume. Please open the URL to see it.",
+                url: `${import.meta.env.VITE_BASE_URL}/my-resume/${resumeId}/view`,
+            })
+            .then(() => console.log('Shared successfully!'))
+            .catch(error => console.log('Error sharing:', error));
+        } else {
+            alert('Web Share API is not supported in your browser.');
+        }
+    }
+
+    return (
+        <ResumeInfoContext.Provider value={{ resumeInfo, setResumeInfo }} >
+            <div id="no-print">
+                <Header />
+
+                <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
+                    <h2 className='text-center text-2xl font-medium'>
+                        Congrats! Your Ultimate AI-generated Resume is ready!
+                    </h2>
+                    <p className='text-center text-gray-400'>
+                        Now you are ready to download your resume and you can share the unique resume URL with your friends and family.
+                    </p>
+                    <div className='flex justify-between px-44 my-10'>
+                        <Button onClick={HandleDownload}>Download</Button>
+                        <Button onClick={HandleShare}>Share</Button>
+                    </div>
+                </div>
+            </div>
+            <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
+                <div id="print-area">
+                    <ResumePreview />
+                </div>
+            </div>
+        </ResumeInfoContext.Provider>
+    )
 }
 
-export default EditResume
+export default ViewResume
