5b72c2c0acc535815e93a63742819146c8b8066a
ankhiwalasmit <ankhiwalasmit@gmail.com>
2024-10-07 01:21:51 +0530

    minor change

--- src/dashboard/resume/components/preview/SkillsPreview.jsx
+++ src/dashboard/resume/components/preview/SkillsPreview.jsx
@@ -7,7 +7,7 @@ function SkillsPreview({resumeInfo}) {
     style={{
         color:resumeInfo?.themeColor
     }}
-    >Summary</h2>
+    >Skills</h2>
     <hr style={{
         borderColor:resumeInfo?.themeColor
     }} />
